bb6694cfa051aa3c5fb60a4df9a9b1e967329967
Nefro9 <nefronix@gmail.com>
2019-02-15 17:03:21 +0200

    Init

---
+++ Source/Apdorok.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Apdorok
+{
+	int [] masyvas;
+	public int [] teigiamiNelyginiai;
+	public int [] masyvasApdorotas;
+
+	
+	public Apdorok(int[] orgMasyvas)
+	{
+		this.masyvas = orgMasyvas;
+		this.ApdorokMasyva();
+	}
+	
+	private void ApdorokMasyva()
+	{
+		teigiamiNelyginiai	= new int[masyvas.Length];
+		masyvasApdorotas	= new int[masyvas.Length];
+		
+		int counterA = 0;
+		int counterB = 0;
+		
+		for (int i = 0; i < masyvas.Length; i++)
+		{
+			if(masyvas[i] >= 0 && masyvas[i] % 2 != 0) {
+				teigiamiNelyginiai[counterA] = masyvas[i];
+				counterA++;
+			} else {
+				masyvasApdorotas[counterB] = masyvas[i];
+				counterB++;
+			}
+		}
+		
+		// Resize array
+		Array .Resize<int >(ref teigiamiNelyginiai, counterA);
+		Array .Resize<int >(ref masyvasApdorotas, counterB);
+	}
+	
+	public int getSum()
+	{
+		int sum = 0;
+		
+		for (int i = 0; i < teigiamiNelyginiai.Length; i++)
+		{
+			sum += teigiamiNelyginiai[i];
+		}
+		
+		return sum;
+	}
+	
+	public float getAvg()
+	{
+		float  avg = 0;
+		
+		if(teigiamiNelyginiai.Length > 0) {
+			avg = (float) this.getSum() / teigiamiNelyginiai.Length;
+		}
+		
+		return avg;
+	}
+}
---
+++ Source/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace NamuDarbai1
+{
+	class Program
+	{
+		
+		public static void Main(string[] args)
+		{
+			// Masyvo sukurimas
+			MasyvasIO ArrayIO 		= new MasyvasIO();
+			
+			int ilgis     = ArrayIO.PildykIlgi();
+			int[] masyvas = ArrayIO.PildykMasyva(ilgis);
+			
+			ArrayIO.SpausdinkMasyva(masyvas);
+			
+			
+			// Masyvo apdorojimas
+			Apdorok ArrayHelper 	= new Apdorok(masyvas);
+			
+			if(ArrayHelper.teigiamiNelyginiai.Length > 0) {
+				Console.WriteLine("");
+				Console.WriteLine("Teigiami nelyginiai elementai");
+				ArrayIO.SpausdinkMasyva(ArrayHelper.teigiamiNelyginiai);
+				
+				Console.WriteLine("Suma: " +ArrayHelper.getSum());
+				Console.WriteLine("Vidurkis: " +ArrayHelper.getAvg().ToString("n2"));
+			} else {
+				Console.WriteLine("");
+				Console.WriteLine("Teigiamu nelyginiu elementu nerasta");
+			}
+			
+			
+			// Masyvo rusiavimas
+			Rikiavimas SortHelper 	= new Rikiavimas(ArrayHelper.masyvasApdorotas);
+			
+			Console.WriteLine("");
+			Console.WriteLine("Originalus masyvas: ");
+			ArrayIO.SpausdinkMasyva(ArrayHelper.masyvasApdorotas);
+			
+			Console.WriteLine("Mazejimo tvarka");
+			ArrayIO.SpausdinkMasyva(SortHelper.RikiuokM());
+			
+			Console.WriteLine("Didejimo tvarka");
+			ArrayIO.SpausdinkMasyva(SortHelper.RikiuokD());
+			
+			Console.ReadKey(true);
+		}
+	}
+}
